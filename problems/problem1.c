6cd0ce9cde4878d58606cd9d8b6266f69a5ecbc9
Prithviraj1217 <55423396+Prithviraj1217@users.noreply.github.com>
2021-03-18 20:18:42 +0530

    Add files via upload

---
+++ problems/problem1.c
@@ -0,0 +1,113 @@
+#include <stdio.h>
+#include <conio.h>
+int main() {
+    syscls();
+	int T , i , count=0;
+	long int n;
+	char name[1000000];
+
+	printf("Enter test cases\n");
+	scanf("%d",&T);
+
+	while(T != 0)
+	{
+	    printf("Enter size of array\n");
+	    scanf("%d",&n);
+
+	    printf("Enter starting letters of %d toys\n",n);
+	    scanf("%s",&name);
+
+	    for(i=0;i<n-1;i++)
+	    {
+	        if(name[i] == name[i+1])
+	        {
+	            count++;
+	        }
+	    }
+
+	    printf("%d\n",count);
+	    T--;
+	    count=0;
+	}
+
+	return 0;
+}
+
+
+//https://www.codechef.com/problems/COINYS
+
+ /*  1 ≤ T≤ 1000
+
+1 ≤ N≤ 100000*/
+
+// SOLUTION_1 FUNCTION
+int solution_1(int size,char *str);
+int solution_1(int size,char *str)
+{
+    int count=0;
+    for(int i=0;i<size-1;i++)
+	    {
+	        if(str[i] == str[i+1])
+	        {
+	            count++;
+	        }
+	    }
+return count;
+}
+
+
+//second solution
+
+#include <stdio.h>
+
+int main(void) {
+	int T;
+	scanf("%d",&T);
+    while(T != 0){
+        int N,i,count=0;
+        scanf("%d",&N);
+        char S[N];
+        scanf("%s",S);
+        for(i=0;i<N;i++){
+        if(S[i] == S[i+1] && S[i] == 'K'){
+            count++;
+        }
+        if(S[i] == S[i+1] && S[i] == 'L'){
+            count++;
+        }
+        if(S[i] == S[i+1] && S[i] == 'M'){
+            count++;
+        }
+        }
+        printf("%d\n",count);
+        T--;
+    }
+	return 0;
+}
+
+//https://www.codechef.com/problems/COINYS
+
+ /*  1 ≤ T≤ 1000
+
+1 ≤ N≤ 100000*/
+//SOLUTION_2 FUNCTION
+
+
+int solution_2(int size,char *string);
+int solution_2(int size,char *string)
+{
+    int count=0;
+    for(int i=0;i<size;i++){
+        if(string[i] == string[i+1] && string[i] == 'K'){
+            count++;
+        }
+        if(string[i] == string[i+1] && string[i] == 'L'){
+            count++;
+        }
+        if(string[i] == string[i+1] && string[i] == 'M'){
+            count++;
+        }
+        }
+return 0;
+}
+
---
+++ problems/problem2_solutions.c
@@ -0,0 +1,236 @@
+#include<stdio.h>
+#include<stdio.h>
+int main()
+{
+   int tt,input,nn,i;
+   char arr[1000000],c,m;
+   printf("Enter test cases\n");
+	scanf("%d",&tt);
+	for(int j=0;j<tt;j++)
+    {
+        scanf("%d",&input);
+
+	nn=input;
+	scanf("%s", arr);
+	if(input%2==1)
+    {
+        input=input-1;
+    }
+
+	for(int i=0;i<input;i=i+2)
+	{
+	    c=arr[i];
+	    arr[i]=arr[i+1];
+	    arr[i+1]=c;
+	}
+
+	for(i=0;i<nn;i=i+1)
+	{
+	   arr[i]=219-arr[i];
+
+	}
+	printf("%s\n",arr);
+
+    }
+
+
+
+}
+//SOLUTION_1 FUNCTION
+
+https://www.codechef.com/problems/CHO1
+
+/*1≤T≤1,000
+
+
+1≤N≤100
+
+
+|S|=N*/
+
+char *solution_1(int len,char *arr);
+char *solution_1(int len,char *arr)
+{
+    char temp;
+    int temp_forsize=len;
+    if(len%2==1)
+    {
+        len-=1;
+    }
+
+	for(int i=0;i<len;i+=2)
+	{
+	    temp=arr[i];
+	    arr[i]=arr[i+1];
+	    arr[i+1]=temp;
+	}
+
+	for(int i=0;i<temp_forsize;i++)
+	{
+	   arr[i]=219-arr[i];
+
+	}
+return arr;
+}
+
+//second solution
+
+#include <stdio.h>
+#include <string.h>
+
+int main(void) {
+
+
+	char arr[100] , temp  ;
+	long int T ;
+	int i  , n , check , k=0 ;
+
+	scanf("%ld",&T);
+
+	while(T != 0)
+	{
+	    scanf("%d",&n);
+
+	    scanf("%s",arr);
+
+	   for(i=0;i<(n/2);i++)
+    {
+        temp = arr[k];
+        arr[k] = arr[k+1];
+        arr[k+1] = temp;
+        k=k+2;
+    }
+
+    for(i=0;i<n;i++)
+    {
+        check = arr[i] - 97;
+        arr[i] = 122 - check;
+    }
+
+    printf("%s\n",arr);
+	   T--;
+	   k=0;
+	}
+
+	return 0;
+}
+//SOLUTION_2 FUNCTION
+
+https://www.codechef.com/problems/CHO1
+
+/*1≤T≤1,000
+
+
+1≤N≤100
+
+
+|S|=N*/
+
+char *solution_2(int len,char *arr);
+char *solution_2(int len,char *arr)
+{
+    int i,n,check,k=0;
+    char temp;
+    for(i=0;i<(n/2);i++)
+    {
+        temp = arr[k];
+        arr[k] = arr[k+1];
+        arr[k+1] = temp;
+        k=k+2;
+    }
+
+    for(i=0;i<n;i++)
+    {
+        check = arr[i] - 97;
+        arr[i] = 122 - check;
+    }
+return arr;
+}
+
+//third solution
+
+#include <stdio.h>
+
+/*void swap(char *p,char *q){
+    char temp=*p;
+    *p=*q;
+    *q=temp;
+}*/
+
+int main(){
+	int T;
+	scanf("%d",&T);
+	while(T!=0){
+	    int N,i;
+	    scanf("%d",&N);
+	    char S[N];
+	    scanf("%s",S);
+	    if(N%2 == 0){
+	        for(i=0;i<N;i++){
+	            swap(&S[i],&S[i+1]);
+	            i++;
+	        }
+	    }
+	    else{
+	        for(i=0;i<N-1;i++){
+	            swap(&S[i],&S[i+1]);
+	            i++;
+	        }
+	    }
+	    int change;
+	    for(i=0;i<N;i++){
+	        change=S[i]-97;
+	        S[i]= 122 - change;
+	    }
+
+	    for(i=0;i<N;i++){
+	        printf("%c",S[i]);
+	    }
+	    printf("\n");
+	    T--;
+	}
+	return 0;
+}
+//SOLUTION_3 FUNCTIONS
+void swap(char *p,char *q){
+    char temp=*p;
+    *p=*q;
+    *q=temp;
+
+}
+
+https://www.codechef.com/problems/CHO1
+
+/*1≤T≤1,000
+
+
+1≤N≤100
+
+
+|S|=N*/
+
+char *solution_3(int len,char *arr);
+char *solution_3(int len,char *arr)
+{
+
+    int i;
+    if(len%2 == 0){
+	        for(i=0;i<len;i++){
+	            swap(&arr[i],&arr[i+1]);
+	            i++;
+	        }
+	    }
+	    else{
+	        for(i=0;i<len-1;i++){
+	            swap(&arr[i],&arr[i+1]);
+	            i++;
+	        }
+	    }
+	    int change;
+	    for(i=0;i<len;i++){
+	        change=arr[i]-97;
+	        arr[i]= 122 - change;
+	    }
+return arr;
+}
+
