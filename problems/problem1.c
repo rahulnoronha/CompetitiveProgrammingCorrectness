29da184b78ad7ccde0db3977bd6ed6f5b00f5deb
Prithviraj1217 <55423396+Prithviraj1217@users.noreply.github.com>
2021-03-18 20:26:44 +0530

    Update problem1.c

--- problems/problem1.c
+++ problems/problem1.c
@@ -1,46 +1,10 @@
-#include <stdio.h>
-#include <conio.h>
-int main() {
-    syscls();
-	int T , i , count=0;
-	long int n;
-	char name[1000000];
+//TOY STORY-> https://www.codechef.com/problems/COINYS
 
-	printf("Enter test cases\n");
-	scanf("%d",&T);
+/*CONSTRAINTS:
+1 ≤ T≤ 1000
+1 ≤ N≤ 100000
+SOLUTION LINK-> https://www.codechef.com/viewsolution/39466541 */
 
-	while(T != 0)
-	{
-	    printf("Enter size of array\n");
-	    scanf("%d",&n);
-
-	    printf("Enter starting letters of %d toys\n",n);
-	    scanf("%s",&name);
-
-	    for(i=0;i<n-1;i++)
-	    {
-	        if(name[i] == name[i+1])
-	        {
-	            count++;
-	        }
-	    }
-
-	    printf("%d\n",count);
-	    T--;
-	    count=0;
-	}
-
-	return 0;
-}
-
-
-//https://www.codechef.com/problems/COINYS
-
- /*  1 ≤ T≤ 1000
-
-1 ≤ N≤ 100000*/
-
-// SOLUTION_1 FUNCTION
 int solution_1(int size,char *str);
 int solution_1(int size,char *str)
 {
@@ -56,42 +20,8 @@ return count;
 }
 
 
-//second solution
-
-#include <stdio.h>
-
-int main(void) {
-	int T;
-	scanf("%d",&T);
-    while(T != 0){
-        int N,i,count=0;
-        scanf("%d",&N);
-        char S[N];
-        scanf("%s",S);
-        for(i=0;i<N;i++){
-        if(S[i] == S[i+1] && S[i] == 'K'){
-            count++;
-        }
-        if(S[i] == S[i+1] && S[i] == 'L'){
-            count++;
-        }
-        if(S[i] == S[i+1] && S[i] == 'M'){
-            count++;
-        }
-        }
-        printf("%d\n",count);
-        T--;
-    }
-	return 0;
-}
-
-//https://www.codechef.com/problems/COINYS
-
- /*  1 ≤ T≤ 1000
-
-1 ≤ N≤ 100000*/
-//SOLUTION_2 FUNCTION
-
+//SECOND SOLUTION
+//SOLUTION LINK-> https://www.codechef.com/viewsolution/39507415
 
 int solution_2(int size,char *string);
 int solution_2(int size,char *string)
